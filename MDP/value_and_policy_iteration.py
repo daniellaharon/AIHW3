94ed20bf315ac38d3534d39f910efbea3fac6c61
daniella aharon <daniella@clairlabs.com>
2022-06-23 08:58:53 +0300

    some bug fixes

--- MDP/value_and_policy_iteration.py
+++ MDP/value_and_policy_iteration.py
@@ -3,9 +3,11 @@ from copy import deepcopy
 import numpy as np
 
 def calcU(mdp,U,state_row,state_col,action):
-    print("-"*30 + f"{state_row, state_col, action}" + "-"*30)
+    # print("-"*30 + f"{state_row, state_col, action}" + "-"*30)
     #Up
     val = 0.0
+    # for action in mdp.transition_function[action]:
+    #     print(action)
     row,col = mdp.step((state_row, state_col), "UP")
     val+= U[row][col] * mdp.transition_function[action][0]
     #down
@@ -17,7 +19,7 @@ def calcU(mdp,U,state_row,state_col,action):
     #right
     row, col = mdp.step((state_row, state_col), "LEFT")
     val += U[row][col] * mdp.transition_function[action][3]
-    return action,val
+    return val
 
 
 
@@ -47,7 +49,7 @@ def value_iteration(mdp, U_init, epsilon=10 ** (-3)):
                 else:
                     U_tag[state_row][state_col] = float(mdp.board[state_row][state_col])
                     U_tag[state_row][state_col] += mdp.gamma* max([calcU(mdp,U,state_row,state_col,s) for s in mdp.actions])
-                _,delta = max(delta, abs(U_tag[state_row][state_col] - U[state_row][state_col]))
+                delta = max(delta, round(float(abs(U_tag[state_row][state_col] - U[state_row][state_col])),4))
         U = deepcopy(U_tag)
         if not mdp.gamma == 1:
             if (delta < (epsilon*(1-mdp.gamma)/mdp.gamma)):
@@ -61,7 +63,7 @@ def getValue(mdp,U,row,col,action,max_row,max_col):
     next_state = tuple(map(sum, zip((row,col), mdp.actions[action])))
     # collide with a wall
     if next_state[0] < 0 or next_state[1] < 0 or next_state[0] >= max_row or next_state[1] >= max_col or \
-            U[next_state[0]][next_state[1]] == None:
+            U[next_state[0]][next_state[1]] == "WALL":
         next_state = (row,col)
     return next_state
 
@@ -78,7 +80,7 @@ def get_policy(mdp, U):
             policy[row][col] = -1
     for row in range(0, np.array(U).shape[0]):
         for col in range(0, np.array(U).shape[1]):
-            if U[row][col] is None:
+            if U[row][col] =="WALL":
                 continue
             max_action , max_u = None, float("-inf")
             for action in mdp.actions:
@@ -98,12 +100,17 @@ def policy_evaluation(mdp, policy):
     #
 
     # ====== YOUR CODE: ======
-    matrix = []
-    for state_row in range(0,np.array(mdp.board).shape[0]):
-        for state_col in range(0,np.array(mdp.board).shape[1]):
-            pass
-    # ========================
+    columns = len(mdp.board[0])
+    rows = len(mdp.board)
+    matrix = np.zeros((rows*columns), (rows*columns))
+    for row in range(0,np.array(mdp.board).shape[0]):
+        for col in range(0,np.array(mdp.board).shape[1]):
+            if not mdp.board[row][col] == "WALL" and not (row, col) in mdp.terminal_states:
+                pass
+            elif mdp.board[row][col] == "WALL":
+                pass
 
+    # ========================
 
 def policy_iteration(mdp, policy_init):
     # TODO:
@@ -113,7 +120,7 @@ def policy_iteration(mdp, policy_init):
     #
 
     # ====== YOUR CODE: ======
-    policy = policy_init
+    policy = deepcopy(policy_init)
     while True:
         unchanged = True
         U = policy_evaluation(mdp,policy)
@@ -124,10 +131,15 @@ def policy_iteration(mdp, policy_init):
                 if (state_row, state_col) in mdp.terminal_states:
                     continue
                 else:
-                    action,_ = max([calcU(mdp,U,state_row,state_col,s) for s in mdp.actions])
+                    orig_val, max_action= float("-inf"), None
+                    for action in mdp.actions:
+                        val = calcU(mdp, U, state_row, state_col, action)
+                        if val > orig_val:
+                            max_action = action
+                            orig_val = val
                     original_action = policy[state_row][state_col]
-                    if action!=original_action:
-                        policy[state_row][state_col] = action
+                    if max_action != original_action:
+                        policy[state_row][state_col] = max_action
                         unchanged = False
         if unchanged:
             return policy
